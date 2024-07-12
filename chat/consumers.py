16e5845eeb48e50e120d5fef77f12d7e11077a30
OnlyYellow <ash1823@naver.com>
2024-07-12 16:24:36 +0900

    fix: 멤버십으로 채팅방 내 멤버 구분

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,20 +1,48 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from urllib.parse import parse_qs
+from .models import ChatRoom
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        user = self.scope["user"]
+        if user.is_authenticated:
+            # 쿼리 스트링에서 room_name 추출
+            query_string = parse_qs(self.scope['query_string'].decode('utf-8'))
+            room_name = query_string.get('room_name', [None])[0]
 
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+            if room_name is None:
+                await self.close()
+                return
+            
+            # 멤버십 검증
+            room = await self.get_room(room_name)
+            if room and await self.is_room_member(room, user):
+                self.room_group_name = f'chat_{room_name}'
 
-        await self.accept()
+                # Join room group
+                await self.channel_layer.group_add(
+                    self.room_group_name,
+                    self.channel_name
+                )
+                await self.accept()
+            else:
+                await self.close()
+        else:
+            await self.close()
+
+    @database_sync_to_async
+    def get_room(self, room_name):
+        try:
+            return ChatRoom.objects.get(name=room_name)
+        except ChatRoom.DoesNotExist:
+            return None
+        
+    @database_sync_to_async
+    def is_room_member(self, room, user):
+        return user in room.members.all()
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -27,21 +55,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        user_email = self.scope["user"].email
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
+                'email': user_email,
                 'message': message
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
+        email = event['email']
         message = event['message']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'email': email,
             'message': message
         }))
--- mysite/asgi.py
+++ mysite/asgi.py
@@ -8,20 +8,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from chat.consumers import ChatConsumer
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
+        URLRouter(
             chat.routing.websocket_urlpatterns
-        ])
+        )
     ),
 })
