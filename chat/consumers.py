0dcb75cf74ae2baf87111615bec91c1fe7cd3ef8
OnlyYellow <ash1823@naver.com>
2024-07-12 14:02:35 +0900

    feat: 채팅방 확인

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,8 +5,8 @@ from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = 'chat_room'
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
 
         # Join room group
         await self.channel_layer.group_add(
--- chat/views.py
+++ chat/views.py
@@ -3,8 +3,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ChatRoomSerializer, AddMemberSerializer
 from .models import ChatRoom
+from authentication.models import User
 from rest_framework import permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class ChatRoomAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        email = request.user.email
+        user = User.objects.get(email=email)
+
+        # 현재 사용자가 멤버인 채팅방만 필터링
+        chat_rooms = ChatRoom.objects.filter(members = user)
+
+        # 채팅방 정보와 각 채팅방의 멤버들의 email을 반환
+        data = [
+            {
+                "room_id": room.id,
+                "name": room.name,
+                "members": [member.email for member in room.members.all()]
+            }
+            for room in chat_rooms
+        ]
+
+        return Response(data, status=status.HTTP_200_OK)
 
 
 class ChatRoomCreateAPIView(APIView):
@@ -14,10 +39,12 @@ class ChatRoomCreateAPIView(APIView):
     def post(self, request):
         serializer = ChatRoomSerializer(data=request.data)
         if serializer.is_valid():
+
             # 채팅방 생성
             chat_room = serializer.save()
             chat_room.members.add(request.user)  # 방 생성자를 자동으로 멤버에 추가
             chat_room.save()
+
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
