b8dd306e06f85f5375bcdad8c2b22090cadf8cdf
OnlyYellow <ash1823@naver.com>
2024-07-12 11:13:34 +0900

    fix: 채팅방 생성 시 멤버 id로 받기

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import ChatRoom
+from authentication.models import User
 
 class ChatRoomSerializer(serializers.ModelSerializer):
+    members = serializers.SlugRelatedField(
+        many=True,
+        slug_field='id',
+        queryset=User.objects.all(),
+        required=False
+    )
+    
     class Meta:
         model = ChatRoom
         fields = ['id', 'name', 'members']
